939e9bc20a745ae837b1fac8fa4ba65e476dd6b6
TULIMAT Anas OBS/GDO <TZKM2460@orange.com>
2023-10-02 15:28:42 +0200

    feat: display Posts list in descnding order the newer to the older

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -45,9 +45,23 @@ const postsSlice = createSlice({
                         title,
                         content,
                         date: new Date().toISOString(),
-                        userId
+                        userId,
+                        reactions: {
+                            thumbsUp: 0,
+                            wow: 0,
+                            heart: 0,
+                            rocket: 0,
+                            coffee: 0
+                        }
                     }
                 }
+            }
+        },
+        reactionAdded(state, action) {
+            const { postId, reaction } = action.payload
+            const existingPost = state.find(post => post.id === postId)
+            if (existingPost) {
+                existingPost.reactions[reaction]++
             }
         }
 
@@ -56,6 +70,9 @@ const postsSlice = createSlice({
 
 export const selectAllPosts = (state) => state.posts;
 
-export const {postAdded} = postsSlice.actions;
+export const {
+    postAdded,
+    reactionAdded
+} = postsSlice.actions;
 
 export default postsSlice.reducer;
